1936aa3332ce8b298d2b5bbf6c1c768a284281fc
desarrollo <desarrollo@desarrollo-ThinkPad-L420>
2020-01-28 12:09:26 -0600

    faces config, tags jsf

---
+++ src/main/java/com/icreams/entity/Sabor.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.icreams.entity;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ *
+ * @author desarrollo
+ */
+public class Sabor implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="idsabor")
+    private Integer idSabor;
+    @Column(name="sabor")
+    private String sabor;
+
+    public Sabor() {
+    }
+
+    public int getIdSabor() {
+        return idSabor;
+    }
+
+    public void setIdSabor(int idSabor) {
+        this.idSabor = idSabor;
+    }
+
+    public String getSabor() {
+        return sabor;
+    }
+
+    public void setSabor(String sabor) {
+        this.sabor = sabor;
+    }
+}
---
+++ src/main/java/com/icreams/entity/Usuario.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.icreams.entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author desarrollo
+ */
+public class Usuario implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idUsuario;
+    @Column(name="nombre")
+    private String nombre;
+    @Column(name="apellido")
+    private String apellido;
+    @Column(name="usuario")
+    private String nomUser;
+    @Column(name="clave")
+    private String clave;
+
+    public Usuario() {
+    }
+
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNomUser() {
+        return nomUser;
+    }
+
+    public void setNomUser(String nomUser) {
+        this.nomUser = nomUser;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+    
+    
+}
